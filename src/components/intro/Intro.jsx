59cedca26515488ff1d7da83da099efa73b03536
keeslucas <dev.engineer114@gmail.com>
2024-04-18 11:50:12 -0700

    Updated letters

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -12,7 +12,6 @@ const Intro = () => {
   return (
     <section id="about">
       <h5>Get to know</h5>
-      <h2>About Me</h2>
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
